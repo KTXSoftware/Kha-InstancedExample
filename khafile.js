3ac2257523d4c8265981d8ac23f049f403349bb9
Robert Konrad <git2016@robdangero.us>
2020-10-22 22:44:58 +0200

    Set the Android API

--- khafile.js
+++ khafile.js
@@ -3,4 +3,6 @@ let project = new Project('Instanced Example');
 project.addSources('Sources');
 project.addShaders('Sources/Shaders/**');
 
+project.addCDefine('KORE_ANDROID_API=18');
+
 resolve(project);
